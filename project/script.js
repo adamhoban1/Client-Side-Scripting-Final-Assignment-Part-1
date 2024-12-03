f04042c0d6d947b6470125d772c5f28096edb8ce
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2024-12-03 11:33:40 +0000

    Update script.js
    update key

--- project/script.js
+++ project/script.js
@@ -1,8 +1,12 @@
-const url = 'https://soundcloud-scraper.p.rapidapi.com/v1/user/tracks?user=https%3A%2F%2Fsoundcloud.com%2Fedsheeran';
+const API_URL = 'https://soundcloud-scraper.p.rapidapi.com/v1/user/tracks?user=https%3A%2F%2Fsoundcloud.com%2Fedsheeran';
+const API_KEY = 'debd88bf4amshf2244f6244ae295p1287a1jsn7557427db93c';
+
+
+const url = API_URL;
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'debd88bf4amshf2244f6244ae295p1287a1jsn7557427db93c',
+		'x-rapidapi-key': API_KEY,
 		'x-rapidapi-host': 'soundcloud-scraper.p.rapidapi.com'
 	}
 };
@@ -15,9 +19,9 @@ try {
     const soundcloudURL = document.getElementById('playlistinput');
 	const button = document.getElementById('search-button');
 
-	button.addEventListener('click'){
-		
-	}
+	button.addEventListener('click')
+
+
 } catch (error) {
 	console.error(error);
 }
