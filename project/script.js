e64f3b0fa1d7d6129b96b0743762c6d06e081526
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2024-12-09 19:08:03 +0000

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-const url = 'https://soundcloud-scraper.p.rapidapi.com/v1/user/tracks?user=https%3A%2F%2Fsoundcloud.com%2Fedsheeran';
-const apiKey = "my key goes hger"
-const data = document.getElementById("info")
-const button = document.getElementById("info")
-=======
 const API_URL = 'https://soundcloud-scraper.p.rapidapi.com/v1/user/tracks?user=https%3A%2F%2Fsoundcloud.com%2Fedsheeran';
 const API_KEY = 'debd88bf4amshf2244f6244ae295p1287a1jsn7557427db93c';
 
@@ -18,7 +12,6 @@ async function searchmusic() {
 
 
 const url = API_URL;
->>>>>>> 40a8e285b370586a6acd20e4ad3e75f6a3beff6f
 const options = {
 	method: 'GET',
 	headers: {
