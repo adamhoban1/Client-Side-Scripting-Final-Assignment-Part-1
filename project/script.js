1269c3158ebe655ecadee6aed679cef367660a5c
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2024-12-09 19:35:49 +0000

    tidied up a bit

--- project/script.js
+++ project/script.js
@@ -1,14 +1,16 @@
 const API_URL = 'https://soundcloud-scraper.p.rapidapi.com/v1/user/tracks?user=https%3A%2F%2Fsoundcloud.com%2Fedsheeran';
 const API_KEY = 'debd88bf4amshf2244f6244ae295p1287a1jsn7557427db93c';
 const searchButton = document.getElementById('search-button');
+const soundCloudURL = document.getElementById('playlistinput');
 
 async function searchmusic() {
 	searchButton.addEventListener('click', () => {
-		const soundCloudURL = document.getElementById('playlistinput').value.trim();
+		const userURL = soundCloudURL.value.trim();
 		if (!soundCloudURL) {
             throw new Error(`Please enter a SoundCloud user URL`);
 			return;
         }
+		newyoke(userURL);
 	});
 
 }
