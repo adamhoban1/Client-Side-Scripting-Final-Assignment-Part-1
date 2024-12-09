39548f97e32973941d414389bbd9f47df03af3ac
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2024-12-09 19:28:09 +0000

    fixed

--- project/script.js
+++ project/script.js
@@ -1,35 +1,18 @@
 const API_URL = 'https://soundcloud-scraper.p.rapidapi.com/v1/user/tracks?user=https%3A%2F%2Fsoundcloud.com%2Fedsheeran';
 const API_KEY = 'debd88bf4amshf2244f6244ae295p1287a1jsn7557427db93c';
+const searchButton = document.getElementById('search-button');
 
 async function searchmusic() {
+	searchButton.addEventListener('click', () => {
+		const soundCloudURL = document.getElementById('playlistinput').value.trim();
+		if (!soundCloudURL) {
+            throw new Error(`Please enter a SoundCloud user URL`);
+			return;
+        }
+	});
 
-	const soundcloudURL = document.getElementById('playlistinput').value.trim();
-	const button = document.getElementById('search-button');
-	const searchButton = document.getElementById('search-button');
-
-    searchButton.addEventListener('click', () => {
 }
 
-
-const url = API_URL;
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': API_KEY,
-		'x-rapidapi-host': 'soundcloud-scraper.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-
-    
-
-	button.addEventListener('click')//test
+   
 
 
-} catch (error) {
-	console.error(error);
-}
