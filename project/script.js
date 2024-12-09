6037e7f26665ed34d78c60fd98f31290c6553e5d
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2024-12-09 20:04:54 +0000

    added main part

--- project/script.js
+++ project/script.js
@@ -3,7 +3,23 @@ const API_KEY = 'debd88bf4amshf2244f6244ae295p1287a1jsn7557427db93c';
 const searchButton = document.getElementById('search-button');
 const soundCloudURL = document.getElementById('playlistinput');
 
-async function searchmusic() {
+async function searchmusic(userURL) {
+	try{
+		const response = await fetch(`${API_URL}${encodeURIComponent(userURL)}`, {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': API_KEY,
+                'X-RapidAPI-Host': 'soundcloud-scraper.p.rapidapi.com'
+            }
+        });
+
+	}
+	catch (error) {
+        const Info = document.getElementById('info');
+        Info.innerHTML = `<p>Error: ${error.message}</p>`;
+	}
+
+	
 	
 
 }
